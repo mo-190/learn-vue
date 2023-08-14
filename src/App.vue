97acbcce90c15d79a9ca104787bc7cf8d08f7c8e
mo-190 <>
2023-08-14 22:45:14 +0800

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="app">
+    <router-view></router-view>
+    <tab-bar></tab-bar>
+  </div>
+</template>
+<script setup>
+import TabBar from "@/components/tab-bar/tab-bar.vue";
+</script>
---
+++ src/components/tab-bar/tab-bar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="tab-bar">
+    <div class="tab-bar-ite">首页</div>
+    <div class="tab-bar-ite">收藏</div>
+    <div class="tab-bar-ite">订单</div>
+    <div class="tab-bar-ite">消息</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="less">
+.tab-bar {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 50px;
+  display: flex;
+  .tab-bar-item {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
