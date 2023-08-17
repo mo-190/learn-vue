06a3ab766f2711cfd4f38ecb5647d62b6367ebb6
mo-190 <mumu1239@163.com>
2023-08-18 00:09:13 +0800

    feat: Home

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,48 +1,36 @@
 <template>
   <div class="tab-bar">
-    <div class="tab-bar-item">
-      <img src="@/assets/img/tabbar/tab_home.png" alt="" />
-      <span class="text">首页</span>
-    </div>
-    <div class="tab-bar-item">
-      <img src="@/assets/img/tabbar/tab_favor.png" alt="" />
-      <span class="text">收藏</span>
-    </div>
-    <div class="tab-bar-item">
-      <img src="@/assets/img/tabbar/tab_order.png" alt="" />
-      <span class="text">订单</span>
-    </div>
-    <div class="tab-bar-item">
-      <img src="@/assets/img/tabbar/tab_message.png" alt="" />
-      <span class="text">消息</span>
-    </div>
+    <van-tabbar v-model="currentIndex" active-color="var(--primary-color)">
+      <template v-for="(item, index) in tabbarData" :key="item.path">
+        <van-tabbar-item :to="item.path">
+          <span>{{ item.text }}</span>
+          <template #icon>
+            <img
+              v-if="currentIndex !== index"
+              :src="getAssetsURL(item.image)"
+              alt=""
+            />
+            <img v-else :src="getAssetsURL(item.imageActive)" alt="" />
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import tabbarData from "@/assets/data/tabbar.js";
+import { getAssetsURL } from "@/utils/load_assets.js";
+import { ref } from "vue";
+
+const currentIndex = ref(0);
+</script>
 
 <style scoped lang="less">
 .tab-bar {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 50px;
-  display: flex;
-  border-top: 1px solid orange;
-  .tab-bar-item {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    img {
-      width: 36px;
-    }
-    .text {
-      margin-top: 2px;
-      font-size: 12px;
-    }
+  --van-tabbar-item-icon-size: 30px !important;
+  img {
+    height: 24px;
   }
 }
 </style>
---
+++ src/views/home/cnps/home-nav-bar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav-bar">
+    <div class="title">弘源旅途</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="less">
+.nav-bar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 46px;
+  border-bottom: 1px solid #f2f2f2;
+}
+.title {
+  color: var(--primary-color);
+  font-size: 16px;
+  font-weight: 600;
+}
+</style>
